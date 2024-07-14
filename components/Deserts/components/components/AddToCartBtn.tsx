d62e9ce26ab388b4b271aa838b790588e95296ba
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-07-14 10:04:39 -0700

    fixed some styling

--- components/Deserts/components/components/AddToCartBtn.tsx
+++ components/Deserts/components/components/AddToCartBtn.tsx
@@ -43,12 +43,12 @@ const AddToCartBtn:React.FC<AddToCartBtnProps> = ({manageCartItem,cartItemInfo,d
     <div className="relative w-full max-w-[300px] mx-7">
     {cartItemInfo?.quantity > 0 ? 
     <div className="py-3 px-4 rounded-full bg-bronze flex items-center justify-between gap-2">
-        <QuantityBtn action="increment" handleQuantityChange={handleQuantityChange} icon="/assets/images/icon-increment-quantity.svg" bronzeIcon="/assets/images/icon-increment-bronze.svg"/>
-          <h5 className="text-white font-semibold transition ease-in">{cartItemInfo.quantity}</h5>
         <QuantityBtn action="decrement" handleQuantityChange={handleQuantityChange} icon="/assets/images/icon-decrement-quantity.svg" bronzeIcon="/assets/images/icon-decrement-bronze.svg"/>
+          <h5 className="text-white font-semibold transition ease-in">{cartItemInfo.quantity}</h5>
+        <QuantityBtn action="increment" handleQuantityChange={handleQuantityChange} icon="/assets/images/icon-increment-quantity.svg" bronzeIcon="/assets/images/icon-increment-bronze.svg"/>
     </div>
           :
-      <button style={{"--i":`${delay}s`} as any} onClick={()=>handleQuantityChange("increment")} className="animate-btn-intro group py-3 w-full rounded-full border-2 border-black hover:border-bronze group/groupitem bg-white flex items-center justify-center gap-2 transition hover:scale-[.98] hover:shadow-no-press-shadow shadow-inset-btn-shadow">
+      <button style={{"--i":`${delay}s`} as any} onClick={()=>handleQuantityChange("increment")} className="animate-btn-intro group py-3 w-full rounded-full border-2 border-black hover:border-bronze group/groupitem bg-white flex items-center justify-center gap-2 transition hover:scale-[.98] shadow-no-press-shadow hover:shadow-inset-btn-shadow">
         <div className="relative w-[20px] h-[20px]">
           <Image src="/assets/images/icon-add-to-cart.svg" alt="add-icon" fill/>
         </div>
